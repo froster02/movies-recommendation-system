6764b33b94a6c4af33c1749eb70b683f3f8608a3
froster02 <arushnaudiyal@gmail.com>
2021-05-31 20:34:19 +0530

    change in styling

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn myApp:app
--- main.py
+++ main.py
@@ -1,53 +1,11 @@
 # ************************************************* RECOMMENDATION SYSTEM *****************************************************************
 
-# IMPORT PYTHON LIBRARIES
 import pandas as pd
 import numpy as np
-import math
-from ast import literal_eval
 
-# READ THE DATA FROM CSV FILE   
-# ds1 = pd.read_csv('/Users/arushnaudiyal/Desktop/testing-movies/Dataset/tmdb_5000_credits.csv')
-# ds2 = pd.read_csv('/Users/arushnaudiyal/Desktop/testing-movies/Dataset/tmdb_5000_movies.csv')
 df = pd.read_csv('/Users/arushnaudiyal/Desktop/testing-movies/Dataset/cleaned_dataset.csv')
 
-# CLEANING DATASET 
-# ds1 = ds1.rename(index=str, columns={"movie_id" : "id"})
-
-# merged = ds1.merge(ds2, on='id')
-
-# merged = merged.drop(columns=['budget','homepage','original_language','title_x','production_companies','production_countries','release_date','revenue','runtime','spoken_languages','tagline','title_y'])
-
-# merged = merged.rename(index=str, columns={"original_title" : "title"})
-
-# features = ['cast', 'crew', 'keywords', 'genres']
-# for feature in features:
-#     merged[feature] = merged[feature].apply(literal_eval)
-
-# def get_director(x):
-#     for i in x:
-#         if i['job'] == 'Director':
-#             return i['name']
-#     return np.nan
-
-# def get_list(x):
-#     if isinstance(x, list):
-#         names = [i['name'] for i in x]
-#         if len(names) > 3:
-#             names = names[:3]
-#         return names
-#     return []
-
-# merged['director'] = merged['crew'].apply(get_director)
-
-# features = ['cast', 'keywords', 'genres']
-# for feature in features:
-#     merged[feature] = merged[feature].apply(get_list)
-
-# merged['index'] = range(0, len(merged))
-
 merged = df
-
 
 # ************************************************* Cosine Similarity *******************************************************************
 
@@ -55,38 +13,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 def get_title(index):
-    # for i in range(0, len(df)):
-    #   if(df.iloc[i]["index"]==index):
-    #     t = df.iloc[i]["title"]
-    # return t
     return df[df.index == index]["title"].values[0]
 
-    # FETH THE VALUES OF RATINGS FROM INDEX
 def get_ratings(index):
-    # for i in range(0, len(df)):
-    #   if(df.iloc[i]["index"]==index):
-    #     r = df.iloc[i]["vote_average"]
-    # return r
     return df[df.index == index]["vote_average"].values[0]
 
-    # FETH THE VALUES OF INDEX FROM TITLE
-# def get_index(title):
-#     for i in range(0,len(merged)):
-#         if(merged.iloc[i]["title"]==title):
-#             index=merged.iloc[i]["index"]
-#     return index
-
 def create_similarity():
-# Apply clean_data function to your features.
-    features = ['cast', 'title', 'director', 'genre']
+    features = ['cast', 'director', 'genre']
 
     for feature in features:
-        # merged[feature] = merged[feature].apply(clean_data)
         df[feature] = df[feature].fillna('')
     def combined_features(row):
-        # return ' '.join(row['keywords']) + ' ' + ' '.join(row['cast']) + ' ' + row['director'] + ' ' + ' '.join(row['genre'])
-        return ' '.join(row['title']) + ' ' + ' '.join(row['cast']) + ' ' + row['director'] + ' ' + ' '.join(row['genre'])
-        # return row['keywords']+" "+row['cast']+" "+row['genre']+" "+row['title']
+        # return ' '.join(row['cast']) + ' ' + row['genre'] + ' ' + ' '.join(row['title'])
+        return row['cast']+" "+row['genre']+" "+row['title']
 
     df['combining'] = df.apply(combined_features, axis=1)
 
@@ -100,9 +39,6 @@ def show_data(movie):
 
     df, cosine_sim = create_similarity()
 
-    # liked_movie = movie        
-    # movie_index = get_index(liked_movie)
-
     try:
         movie_index = df.loc[df['title']==movie].index[0]
     except:
@@ -110,10 +46,8 @@ def show_data(movie):
 
     i = int(movie_index)
 
-        # Give index to values and then typecase to list
     similar_movies = list(enumerate(cosine_sim[i]))
         
-        # sorted list of the specified iterable object
     sorted_similar_movies = sorted(similar_movies,key = lambda x:x[1], reverse = True)        
                     
     i=0
@@ -129,8 +63,7 @@ def show_data(movie):
         List1[j]=s
         List2[j]=r
 
-        j=j+1
-            
+        j=j+1 
         i=i+1
         if i>=10:
             break
@@ -192,23 +125,17 @@ def rate(ch1):
 
 
 #****************************************************** GENRES ******************************************************************************
-
+df1 = df
 def gen(ch2):
 
-    merged['genres'] = merged['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x, list) else [])
-
-    s = merged.apply(lambda x: pd.Series(x['genres']),axis=1).stack().reset_index(level=1, drop=True)
-    s.name = 'genre'
-    gen_md = merged.drop('genres', axis=1).join(s)
-
-    df1 = gen_md[gen_md['genre'] == ch2]
+    df1 = df.loc[df['genre']==ch2].index[0]
 
     v = df1['vote_count']
     r = df1['vote_average']
     C = r.mean()
     m = v.quantile(0.95)
     
-    qualified = df1[(v >= m) & (v.notnull()) & (r.notnull())][['title', 'vote_count', 'vote_average', 'popularity']]
+    qualified = df1[(v >= m) & (v.notnull()) & (r.notnull())][['title', 'vote_count', 'vote_average']]
     qualified['vote_count'] = qualified['vote_count'].astype('int')
     qualified['vote_average'] = qualified['vote_average'].astype('int')
     
