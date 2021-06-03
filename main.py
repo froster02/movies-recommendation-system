e22b94675f6f35af46b4af9a16163ed777163071
froster02 <arushnaudiyal@gmail.com>
2021-06-03 18:26:58 +0530

    testing mogoDB to get data

--- main.py
+++ main.py
@@ -1,130 +1,163 @@
-# ************************************************* RECOMMENDATION SYSTEM *****************************************************************
+# # ************************************************* RECOMMENDATION SYSTEM *****************************************************************
 
-import pandas as pd
-import numpy as np
+# from operator import index
+# import pandas as pd
+# import numpy as np
+# from config import *
 
-df = pd.read_csv('new_cleaned_dataset.csv')
+# # ************************************************** (REVERT CHANGES) ******************************
 
-# id|crew|keywords|title|overview|popularity|release_date|status|tagline|vote_average|vote_count|genre|director|index|cast
+# df = pd.read_csv('new_cleaned_dataset.csv')
 
-merged = df
+# # id|crew|keywords|title|overview|popularity|release_date|status|tagline|vote_average|vote_count|genre|director|index|cast
 
-# ************************************************* Cosine Similarity *******************************************************************
+# # ************************************************* Cosine Similarity *******************************************************************
 
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+# from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
 
-def fun2_enumerate(sequence, start=0):
-    n = start
-    for elem in sequence:
-        yield n, elem
-        n += 1
+# def fun1_fillna(x):
+#     if isinstance(x, list):
+#         return [str(i.replace("NaN", "")) for i in x]
+#     else:
+#         if isinstance(x, str):
+#             return str(x.replace("NaN", ""))
+#         else:
+#             return ''
 
-def create_similarity():
-    features = ['cast', 'keywords', 'director', 'genre']
+# def fun2_enumerate(sequence, start=0):
+#     n = start
+#     for elem in sequence:
+#         yield n, elem
+#         n += 1
 
-    for feature in features:
-        df[feature] = df[feature].fillna('')
-    def combined_features(row):
-        return row['cast'] + " " + row['keywords'] + " " + row['director']+ " " +row['genre']
+# def create_similarity():
+#     features = ['cast', 'keywords', 'director', 'genre']
 
-    df['combining'] = df.apply(combined_features, axis=1)
+#     for feature in features:
+#         df[feature] = df[feature].apply(fun1_fillna)
+#     def combined_features(row):
+#         return row['cast'] + " " + row['keywords'] + " " + row['director']+ " " +row['genre']
 
-    count = CountVectorizer()
-    count_matrix = count.fit_transform(df['combining'])
+#     df['combining'] = df.apply(combined_features, axis=1)
 
-    cosine_sim = cosine_similarity(count_matrix)
-    return df, cosine_sim
+#     count = CountVectorizer()
+#     count_matrix = count.fit_transform(df['combining'])
+#     cosine_sim = cosine_similarity(count_matrix)
+
+#     return df, cosine_sim
+
+# def detail(detail_index):
+
+#     movie_idx = df.loc[df['title']==detail_index].index[0]
+#     i = int(movie_idx)
+
+#     title_list1 =[None]*1    
+#     rating_list1 =[None]*1
+
+#     s = df[df.index == index]["title"].values[0]
+#     r = df[df.index == index]["vote_average"].values[0]
+
+#     title_list1[0]=s
+#     rating_list1[0]=r
+
+#     return (title_list1, rating_list1)
+
   
-def show_data(movie):
+# def show_data(movie):
 
-    df, cosine_sim = create_similarity()
+#     df, cosine_sim = create_similarity()
 
-    try:
-        movie_index = df.loc[df['title']==movie].index[0]
-    except:
-        return['Sorry! The movie you requested is not in our database. Please check the spelling or try with other movies!'], ['¯\_(ツ)_/¯']
+#     try:
+#         movie_index = df.loc[df['title']==movie].index[0]
+#     except:
+#         return['Sorry! The movie you requested is not in our database. Please check the spelling or try with other movies!'], ['¯\_(ツ)_/¯']
 
-    i = int(movie_index)
+#     i = int(movie_index)
 
-    similar_movies = list(fun2_enumerate(cosine_sim[i]))
+#     similar_movies = list(fun2_enumerate(cosine_sim[i]))
         
-    sorted_similar_movies = sorted(similar_movies,key = lambda x:x[1], reverse = True)  
+#     sorted_similar_movies = sorted(similar_movies,key = lambda x:x[1], reverse = True)  
 
-    sorted_similar_movies = sorted_similar_movies[1:11]      
+#     # sorted_similar_movies = sorted_similar_movies[1:11]      
                     
-    i=0
-    j=0
-    List1 =[None]*10    
-    List2 =[None]*10
+#     i=0
+#     j=0
 
-    for element in sorted_similar_movies:
+#     title_list1 =[None]*10    
+#     rating_list1 =[None]*10
+#     detail_list1 =[None]*10
+
+#     for element in sorted_similar_movies:
                 
-        # s = get_title(element[0])
-        index = element[0]
-        s = df[df.index == index]["title"].values[0]
-        r = df[df.index == index]["vote_average"].values[0]
+#         # s = get_title(element[0])
+#         index = element[0]
+#         s = df[df.index == index]["title"].values[0]
+#         r = df[df.index == index]["vote_average"].values[0]
+#         idx = df[df.index == index]["index"].values[0]
+ 
+#         title_list1[j]=s
+#         rating_list1[j]=r
+#         detail_list1[j]=idx
+
+#         j=j+1 
+#         i=i+1
+#         if i>=10:
+#             break
                 
-        List1[j]=s
-        List2[j]=r
+#     return (title_list1, rating_list1, detail_list1)
 
-        j=j+1 
-        i=i+1
-        if i>=10:
-            break
-                
-    return (List1, List2)
+# # ************************************************* WEIGHTED AVERAGE *********************************************************************************
 
-# ************************************************* WEIGHTED AVERAGE *********************************************************************************
+# def top(choice):
+#     res = 0
+#     v = df['vote_count'] 
+#     R = df['vote_average'] 
+#     C = df['vote_average'].mean() 
+#     m = df['vote_count'].quantile(0.70) 
 
-def top(choice):
-    res = 0
-    v = df['vote_count'] 
-    R = df['vote_average'] 
-    C = df['vote_average'].mean() 
-    m = df['vote_count'].quantile(0.70) 
+#     res = ((R*v)+ (C*m))/(v+m)
 
-    res = ((R*v)+ (C*m))/(v+m)
+#     df['weighted_average'] = res
 
-    df['weighted_average'] = res
+#     movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
 
-    movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
+#     title = movie_sorted_ranking['title'].head(choice)
+#     ratings = movie_sorted_ranking['vote_average'].head(choice)
 
-    title = movie_sorted_ranking['title'].head(choice)
-    ratings = movie_sorted_ranking['vote_average'].head(choice)
+#     list3 = title.values.tolist()
+#     list4 = ratings.values.tolist()
 
-    list3 = title.values.tolist()
-    list4 = ratings.values.tolist()
+#     return list3, list4
 
-    return list3, list4
+# # ***************************************************** RATING ************************************************************
 
-# ***************************************************** RATING ************************************************************
+# def rate(ch1):
 
-def rate(ch1):
+#     res = 0
+#     v = df['vote_count'] 
+#     R = df['vote_average'] 
+#     C = df['vote_average'].mean() 
+#     m = df['vote_count'].quantile(0.70) 
 
-    res = 0
-    v = df['vote_count'] 
-    R = df['vote_average'] 
-    C = df['vote_average'].mean() 
-    m = df['vote_count'].quantile(0.70) 
+#     res = round(((R*v)+ (C*m))/(v+m),1)
 
-    res = round(((R*v)+ (C*m))/(v+m),1)
+#     df['weighted_average'] = res
 
-    df['weighted_average'] = res
-
-    movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
+#     movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
     
-    i=0
-    List5 =[]    
-    List6 =[]
+#     i=0
+#     List5 =[]    
+#     List6 =[]
 
-    for element in range(0, len(movie_sorted_ranking)):
-        if ((movie_sorted_ranking.iloc[element]['weighted_average'] >= ch1) and (movie_sorted_ranking.iloc[element]['weighted_average'] <= ch1 + 0.9)):
-            List5.append(movie_sorted_ranking.iloc[element]['title'])
-            List6.append(movie_sorted_ranking.iloc[element]['weighted_average'])
+#     for element in range(0, len(movie_sorted_ranking)):
+#         if ((movie_sorted_ranking.iloc[element]['weighted_average'] >= ch1) and (movie_sorted_ranking.iloc[element]['weighted_average'] <= ch1 + 0.9)):
+#             List5.append(movie_sorted_ranking.iloc[element]['title'])
+#             List6.append(movie_sorted_ranking.iloc[element]['weighted_average'])
 
-            i=i+1
-            if i>=10:
-                break    
-    return (List5, List6)
+#             i=i+1
+#             if i>=10:
+#                 break    
+#     return (List5, List6)
     
+# *****************************testing for mongoDB****************************
--- myApp.py
+++ myApp.py
@@ -2,14 +2,22 @@
 
 from flask import Flask, render_template, request
 from numpy import tile
-from main import show_data, top, rate
+from main import show_data, top, rate, detail
 
 app = Flask(__name__)
 
 # route() function of the Flask class is a decorator, which tells the application which URL should call the associated function.
 @app.route('/',methods = ['GET'])
 def show_index_html():
+    # return render_template('index.html', movies=[], ratings=[])
     return render_template('index.html', movies=[], ratings=[])
+
+# post SEARCH
+@app.route('/send_data', methods = ['POST'])
+def get_data_from_html():
+        play = request.form['play']
+        movies,ratings,detail = show_data(play)
+        return render_template('SearchResult.html', movies = movies, ratings=ratings, detail=detail)
 
 # post HITS
 @app.route('/get_top', methods = ['POST'])
@@ -27,20 +35,13 @@ def show_ratings():
     titles,ratings = rate(ch1)
     return render_template('index.html', titles = titles, ratings=ratings)
 
-# post GENRES
-# @app.route('/getG', methods = ['POST'])
-# def show_genre():
-#     ch2 = request.form['gen']
-#     ch2 = str(ch2)
-#     titles,ratings = gen(ch2)
-#     return render_template('index.html', titles = titles, ratings=ratings)
-
-# post SEARCH
-@app.route('/send_data', methods = ['POST'])
-def get_data_from_html():
-        play = request.form['play']
-        movies,ratings = show_data(play)
-        return render_template('index.html', movies = movies, ratings=ratings)
+# post DETAIL
+@app.route('/getD', methods = ['POST'])
+def show_details():
+    ch2 = request.form['detail']
+    ch2 = int(ch2)
+    titles,ratings = detail(ch2)
+    return render_template('details.html', titles = titles, ratings=ratings)
 
 if __name__ == '__main__':
     app.run(debug=True)
