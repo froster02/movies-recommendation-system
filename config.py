9b0b61b3f5a75e4230cc025d8845a417677b6398
froster02 <arushnaudiyal@gmail.com>
2021-06-05 07:53:01 +0530

    testing python processing using mongoDB

--- config.py
+++ config.py
@@ -6,62 +6,75 @@ try:
 except:
     print("connection error")
 
+# print("loading...")
+
 db = client["db_movie"]
-col = db["col_movie"]
+col = db["new_col_movie"]
 
 # TITLE
 t = col.find({},{'title': 1})
 title = []
 for data in t:
 	title.append(data['title'])
-print("1-----------------------------------------------------------------------")
-# print(title)
 
 # KEYWORDS
 k = col.find({},{'keywords': 1})
 keywords = []
 for data in k:
 	keywords.append(data['keywords'])
-print("2-----------------------------------------------------------------------")
-# print(keywords)
 
 # DIRECTOR
 d = col.find({},{'director': 1})
 director = []
 for data in d:
 	director.append(data['director'])
-print("3-----------------------------------------------------------------------")
-# print(director)
 
 # GENRE
 g = col.find({},{'genre': 1})
 genre = []
 for data in g:
 	genre.append(data['genre'])
-print("4-----------------------------------------------------------------------")
-# print(genre)
 
 # VOTE_AVERAGE
 va = col.find({},{'vote_average': 1})
 vote_average = []
 for data in va:
 	vote_average.append(data['vote_average'])
-print("5-----------------------------------------------------------------------")
-# print(vote_average)
 
 # VOTE_COUNT
 vc = col.find({},{'vote_count': 1})
 vote_count = []
 for data in vc:
 	vote_count.append(data['vote_count'])
-print("6-----------------------------------------------------------------------")
-# print(vote_count)
 
 # CAST
 c = col.find({},{'cast': 1})
 cast = []
 for data in c:
 	cast.append(data['cast'])
-print("7-----------------------------------------------------------------------")
-# print(cast)
-print("OK!")
+
+# WEIGHTED AVERAGE
+wa = col.find({},{'weighted_average' : 1})
+weighted_average = []
+for data in wa:
+	weighted_average.append(data['weighted_average'])
+
+# COMBINING
+co = col.find({}, {'combining' : 1})
+combining = []
+for data in co:
+	combining.append(data['combining'])
+
+# INDEX
+i = col.find({}, {'index' : 1})
+index = []
+for data in i:
+	index.append(data['index'])
+
+# OVERVIEW
+ov = col.find({}, {'overview' : 1})
+overview = []
+for data in ov:
+	overview.append(data['overview'])
+
+print("*** Connected Successfully to MongoDB Cluster ***")
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # ************************************************* RECOMMENDATION SYSTEM *****************************************************************
 
 # from operator import index
-import pandas as pd
-# from config import *
+import enum
+# import pandas as pd
+from config import *
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 # ************************************************** (REVERT CHANGES) ******************************
 
-df = pd.read_csv('testing_dataset.csv', low_memory=False)
+# df = pd.read_csv('testing_dataset.csv', low_memory=False)
 
 # id|crew|keywords|title|overview|popularity|release_date|status|tagline|vote_average|vote_count|genre|director|index|cast
 
@@ -23,41 +24,49 @@ df = pd.read_csv('testing_dataset.csv', low_memory=False)
 #         else:
 #             return ''
 
-def fun2_enumerate(sequence, start=0):
-    n = start
-    for elem in sequence:
-        yield n, elem
-        n += 1
+# def fun2_enumerate(sequence, start=0):
+#     n = start
+#     for elem in sequence:
+#         yield n, elem
+#         n += 1
 
-def create_similarity():
-    features = ['cast', 'keywords', 'director', 'genre']
+# def create_similarity():
+#     # features = ['cast', 'keywords', 'director', 'genre']
 
-    # for feature in features:
-    #     df[feature] = df[feature].apply(fun1_fillna)
-    # def combined_features(row):
-    #     return row['cast'] + " " + row['keywords'] + " " + row['director']+ " " +row['genre']
+#     # for feature in features:
+#     #     df[feature] = df[feature].apply(fun1_fillna)
+#     # def combined_features(row):
+#     #     return row['cast'] + " " + row['keywords'] + " " + row['director']+ " " +row['genre']
 
-    # df['combining'] = df.apply(combined_features, axis=1)
+#     # df['combining'] = df.apply(combined_features, axis=1)
 
-    count = CountVectorizer()
-    count_matrix = count.fit_transform(df['combining'])
-    cosine_sim = cosine_similarity(count_matrix)
+#     # count = CountVectorizer()
+#     # count_matrix = count.fit_transform(df['combining'])
+#     # cosine_sim = cosine_similarity(count_matrix)
 
-    return df, cosine_sim
+#     # return df, cosine_sim
+#     cm = CountVectorizer().fit_transform(combining)
+#     cs = cosine_similarity(cm)
+
+#     return cs
   
 def show_data(movie):
 
-    df, cosine_sim = create_similarity()
+    cm = CountVectorizer().fit_transform(combining)
+    cs = cosine_similarity(cm)
+    # df, cosine_sim = create_similarity()
 
     try:
         # movie_index = df.loc[df['title']==movie].index[0]
-        movie_index = df[df['title']==movie].index[0]
+        # movie_index = df[df['title']==movie].index[0]
+        movie_index = title.index(movie)
     except:
-        return['Sorry! The movie you requested is not in our database. Please check the spelling or try with other movies!'], ['¯\_(ツ)_/¯']
+        return['Sorry! The movie you requested is not in our database. Please check the spelling or try with other movies!'], ['¯\_(ツ)_/¯'], []
 
-    i = int(movie_index)
+    # i = int(movie_index)
 
-    similar_movies = list(fun2_enumerate(cosine_sim[i]))
+    # similar_movies = list(fun2_enumerate(cosine_sim[i]))
+    similar_movies = list(enumerate(cs[movie_index]))
         
     sorted_similar_movies = sorted(similar_movies,key = lambda x:x[1], reverse = True)  
 
@@ -66,21 +75,33 @@ def show_data(movie):
     i=0
     j=0
 
-    title_list1 =[None]*10    
-    rating_list1 =[None]*10
-    detail_list1 =[None]*10
+    # title_list1 =[None]*10    
+    # rating_list1 =[None]*10
+    # detail_list1 =[None]*10
+
+    title_list1 =[]   
+    rating_list1 =[]
+    detail_list1 =[]
 
     for element in sorted_similar_movies:
                 
         # s = get_title(element[0])
-        index = element[0]
-        s = df[df.index == index]["title"].values[0]
-        r = df[df.index == index]["vote_average"].values[0]
-        idx = df[df.index == index]["index"].values[0]
+        # idx = element[0]
+        # s = df[df.index == index]["title"].values[0]
+        # r = df[df.index == index]["vote_average"].values[0]
+        # idx = df[df.index == index]["index"].values[0]
+        
+        movie_title = title[element[0]]
+        movie_rating = vote_average[element[0]]
+        # movie_idx = index[element[0]]
+
+        title_list1.append(movie_title)
+        rating_list1.append(movie_rating)
+        detail_list1.append(element[0])
  
-        title_list1[j]=s
-        rating_list1[j]=r
-        detail_list1[j]=idx
+        # title_list1[j]=s
+        # rating_list1[j]=r
+        # detail_list1[j]=idx
 
         j=j+1 
         i=i+1
@@ -90,10 +111,10 @@ def show_data(movie):
     return (title_list1, rating_list1, detail_list1)
 
 def send_detail(index):
-    t = df[df.index == index]["title"].values[0]
-    d = df[df.index == index]["director"].values[0]
-    r = df[df.index == index]["vote_average"].values[0]
-    o = df[df.index == index]["overview"].values[0]
+    t = title[index]
+    d = director[index]
+    r = vote_average[index]
+    o = overview[index]
     return (t, d, r, o)
 
 # ************************************************* WEIGHTED AVERAGE *********************************************************************************
@@ -109,44 +130,60 @@ def top(choice):
 
     # df['weighted_average'] = res
 
-    movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
+    # movie_sorted_ranking=sorted(weighted_average,ascending=False)
+    # movie_sorted_ranking=sorted(weighted_average,reverse=True)
 
-    title = movie_sorted_ranking['title'].head(choice)
-    ratings = movie_sorted_ranking['vote_average'].head(choice)
+    # title = movie_sorted_ranking['title'].head(choice)
+    # ratings = movie_sorted_ranking['vote_average'].head(choice)
 
-    list3 = title.values.tolist()
-    list4 = ratings.values.tolist()
+    # list3 = title.values.tolist()
+    # list4 = ratings.values.tolist()
+    list3 = []
+    list2 = []
+    temp = []
 
-    return list3, list4
+    list3 = weighted_average
+    temp = [x for _,x in sorted(zip(weighted_average, title), key=lambda pair : pair[0])]
+    list3.sort(reverse=True)
+
+    for i in range(0, choice):
+        list2.append(temp[i])
+
+    return list2, list3
 
 # ***************************************************** RATING ************************************************************
 
 def rate(ch1):
 
-    res = 0
-    v = df['vote_count'] 
-    R = df['vote_average'] 
-    C = df['vote_average'].mean() 
-    m = df['vote_count'].quantile(0.70) 
+    # res = 0
+    # v = df['vote_count'] 
+    # R = df['vote_average'] 
+    # C = df['vote_average'].mean() 
+    # m = df['vote_count'].quantile(0.70) 
 
-    res = round(((R*v)+ (C*m))/(v+m),1)
+    # res = round(((R*v)+ (C*m))/(v+m),1)
 
-    df['weighted_average'] = res
+    # df['weighted_average'] = res
 
-    movie_sorted_ranking=df.sort_values('weighted_average',ascending=False)
+    # movie_sorted_ranking=sorted(weighted_average,ascending=False)
+    # movie_sorted_ranking = sorted(weighted_average,reverse=True)
     
     i=0
-    List5 =[]    
+    # List5 =[]    
     List6 =[]
+    temp = []
 
-    for element in range(0, len(movie_sorted_ranking)):
-        if ((movie_sorted_ranking.iloc[element]['weighted_average'] >= ch1) and (movie_sorted_ranking.iloc[element]['weighted_average'] <= ch1 + 0.9)):
-            List5.append(movie_sorted_ranking.iloc[element]['title'])
-            List6.append(movie_sorted_ranking.iloc[element]['weighted_average'])
+    for element in weighted_average :
+        if ( float(element) >= ch1 and float(element) <= ch1 + 0.9):
+            # List5.append(movie_sorted_ranking.iloc[element]['title'])
+            # List6.append(movie_sorted_ranking.iloc[element]['weighted_average'])
+            temp.append(title[weighted_average.index(element)])
+            List6.append(element)
+        i=i+1
+        if i>=10:
+            break 
 
-            i=i+1
-            if i>=10:
-                break    
-    return (List5, List6)
+
+    return (temp, List6)
     
 # *****************************testing for mongoDB****************************
